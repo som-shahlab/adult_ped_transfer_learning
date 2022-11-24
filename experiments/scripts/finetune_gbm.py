ad7f1eb97d135571c252d01bb3b06f1144f75142
joshualemmon <joshua.lemmon@uoit.net>
2022-11-24 12:00:00 -0800

    fixing slurm script

--- experiments/scripts/finetune_gbm.py
+++ experiments/scripts/finetune_gbm.py
@@ -166,7 +166,6 @@ def eval_model(args, task, m, model_path, result_path, X_test, y_test, hp):
 		patient_id_var='prediction_id',
 		return_result_df = True
 	)
-	os.makedirs(f'results_save_fpath/lr_{hp["learning_rate"]}_nl_{hp["num_leaves"]}_bt_{hp["boosting_type"]}',exist_ok=True)
 	
 	df_test['lr'] = hp['learning_rate']
 	df_test['leaves'] = hp['num_leaves']
@@ -217,7 +216,7 @@ for cohort_type in ['adult']:
 
 		ft_model_path = f'{args.model_path}/{cohort_type}/gbm_ft/{task}/{feat_group}_feats/best'
 		os.makedirs(ft_model_path,exist_ok=True)
-
+		
 		ft_result_path = f'{args.result_path}/{cohort_type}/gbm_ft/{task}/{feat_group}_feats/best'
 		os.makedirs(ft_result_path,exist_ok=True)
 
--- experiments/scripts/run_gbm_models.sh
+++ experiments/scripts/run_gbm_models.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=joshua.lemmon@sickkids.ca
-#SBATCH --time=5-00:00 # Runtime in D-HH:MM
-#SBATCH --job-name=tl_lr_models
+#SBATCH --time=4-00:00 # Runtime in D-HH:MM
+#SBATCH --job-name=ft_gbm_models
 #SBATCH --nodes=1 
 #SBATCH -n 16 #number of cores to reserve, default is 1
-#SBATCH --mem=50000 # in MegaBytes. default is 8 GB
+#SBATCH --mem=30000 # in MegaBytes. default is 8 GB
 #SBATCH --partition=shahlab # Partition allocated for the lab
-#SBATCH --error=logs/error-sbatchjob.%J.err
-#SBATCH --output=logs/out-sbatchjob.%J.out
+#SBATCH --error=/labs/shahlab/projects/jlemmon/transfer_learning/experiments/logs/error-sbatchjob.%J.err
+#SBATCH --output=/labs/shahlab/projects/jlemmon/transfer_learning/experiments/logs/out-sbatchjob.%J.out
 
 #source activate /local-scratch/nigam/envs/jlemmon/conl 
 source activate /home/jlemmon/.conda/envs/tl
@@ -16,9 +16,11 @@ source activate /home/jlemmon/.conda/envs/tl
 #cd /local-scratch/nigam/projects/jlemmon/transfer_learning/experiments/scripts 
 cd /labs/shahlab/projects/jlemmon/transfer_learning/experiments/scripts
 
+
 # make log folders if not exist
-#mkdir -p ../logs/train_lr
-#mkdir -p ../logs/test_lr
+#mkdir -p ../logs/train_gbm
+#mkdir -p ../logs/test_gbm
+#mkdir -p ../logs/ft_gbm
 
 ## -----------------------------------------------------------
 ## --------------------- job specification -------------------
@@ -31,10 +33,6 @@ N_BOOT=1000
 # number of pipes to execute in parallel
 # this will exec $N_JOBS * $N_TASKS jobs in parallel
 N_JOBS=1
-
-# whether to re-run 
-TRAIN_OVERWRITE='False'
-EVAL_OVERWRITE='False'
 
 ## -----------------------------------------------------------
 ## ----------------------- job pipeline ----------------------
@@ -51,54 +49,52 @@ JOB_ID=$(cat /proc/sys/kernel/random/uuid)
 # define pipeline
 function pipe {
 
-    # Training LR models
-    # executes $N_TASK jobs in parallel
-    #echo "TRAINING MODELS..."
-    #local k=0
-   # for (( ij=0; ij<$N_COHORTS; ij++ )); do
-  #  	for (( g=0; g<$N_GROUPS; g++)); do
+	#Training GBM models
+	#executes $N_TASK jobs in parallel
+# 	echo "TRAINING MODELS..."
+# 	local k=0
+# 	for (( ij=0; ij<$N_COHORTS; ij++ )); do
+# 	for (( g=0; g<$N_GROUPS; g++)); do
 
- #       	for (( t=0; t<$N_TASKS; t++ )); do
-#
-#				python -u train_gbm.py \
-#					--task=${TASKS[$t]} \
-#					--cohort_type=${COHORT_TYPES[$ij]} \
-#					--feat_group=${FEAT_GROUPS[$g]} \
-#					--bin_path="$1" \
-#					--cohort_path="$2" \
-#					--hparam_path="$3" \
-#					--model_path="$4" \
-#					--results_path="$5" #\
-#					#>> "../logs/train_lr/${1:2:2}-${1: -2}-${TASKS[$t]}-$JOB_ID" &
-#
-#
-#				let k+=1
-#				[[ $((k%N_TASKS)) -eq 0 ]] && wait
-#	        done
-    #    done
-    #done
-    #echo "EVALUATING MODELS..."
-    # evaluate models
-    # executes $N_TASK jobs in parallel
-  #  local k=0
- #   for (( t=0; t<$N_TASKS; t++ )); do
-#		python -u test_gbm.py \
-#			   --task=${TASKS[$t]} \
-#			   --bin_path="$1" \
-#			   --cohort_path="$2" \
-#			   --hparam_path="$3" \
-#			   --model_path="$4" \
-#			   --result_path="$5" #\
-#			   #>> "../logs/test_lr/${1:2:2}-${1: -2}-${TASKS[$t]}-$JOB_ID" &
-#		let k+=1
-#		[[ $((k%N_TASKS)) -eq 0 ]] && wait
-   #  done
+# 		for (( t=0; t<$N_TASKS; t++ )); do
 
-    echo "FINETUNING MODELS..."
-    # finetune models
-    # executes $N_TASK jobs in parallel
-    local k=0
-    for (( t=0; t<$N_TASKS; t++ )); do
+# 				python -u train_gbm.py \
+# 					--task=${TASKS[$t]} \
+# 					--cohort_type=${COHORT_TYPES[$ij]} \
+# 					--feat_group=${FEAT_GROUPS[$g]} \
+# 					--bin_path="$1" \
+# 					--cohort_path="$2" \
+# 					--hparam_path="$3" \
+# 					--model_path="$4" \
+# 					--results_path="$5" #\
+# 					#>> "../logs/train_gbm/${1:2:2}-${1: -2}-${TASKS[$t]}-$JOB_ID" &
+# 				let k+=1
+# 				[[ $((k%N_TASKS)) -eq 0 ]] && wait
+# 			done
+# 	   done
+# 	done
+# 	echo "EVALUATING MODELS..."
+# 	#evaluate models
+# 	#executes $N_TASK jobs in parallel
+# 	local k=0
+# 	for (( t=0; t<$N_TASKS; t++ )); do
+# 		python -u test_gbm.py \
+# 			   --task=${TASKS[$t]} \
+# 			   --bin_path="$1" \
+# 			   --cohort_path="$2" \
+# 			   --hparam_path="$3" \
+# 			   --model_path="$4" \
+# 			   --result_path="$5" #\
+# 			   #>> "../logs/test_gbm/${1:2:2}-${1: -2}-${TASKS[$t]}-$JOB_ID" &
+# 		let k+=1
+# 		[[ $((k%N_TASKS)) -eq 0 ]] && wait
+# 	done
+
+	echo "FINETUNING MODELS..."
+	# finetune models
+	# executes $N_TASK jobs in parallel
+	local k=0
+	for (( t=0; t<$N_TASKS; t++ )); do
 		python -u finetune_gbm.py \
 			   --task=${TASKS[$t]} \
 			   --bin_path="$1" \
@@ -106,10 +102,10 @@ function pipe {
 			   --hparam_path="$3" \
 			   --model_path="$4" \
 			   --result_path="$5" #\
-			   #>> "../logs/test_lr/${1:2:2}-${1: -2}-${TASKS[$t]}-$JOB_ID" &
+			   #>> "../logs/ft_gbm/${1:2:2}-${1: -2}-${TASKS[$t]}-$JOB_ID" &
 		let k+=1
 		[[ $((k%N_TASKS)) -eq 0 ]] && wait
-     done
+	 done
 
 }
 
